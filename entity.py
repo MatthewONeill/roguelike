f145b92fe64ff5f6bed313b6869f8410761b5744
Matthew <matthewjo30@gmail.com>
2020-10-10 19:11:55 -0400

    map

---
+++ entity.py
@@ -0,0 +1,17 @@
+from typing import Tuple
+
+class Entity:
+    #Generic Object to represent players, enemies, items, etc
+
+    def __init__(self, x: int, y: int, char: str, color: Tuple[int, int, int]):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move(self, dx: int, dy: int) -> None:
+        #move entity
+        self.x += dx
+        self.y += dy
+
+    
---
+++ tile_types.py
@@ -0,0 +1,36 @@
+from typing import Tuple
+
+import numpy as np
+
+graphic_dt = np.dtype( #dtype creates a data type which numpy can use, behaves similar to a struct
+    [
+        ("ch", np.int32), #character represented in int form
+        ("fg", "3B"), #foreground color represented in 3 unsigned bytes (rgb color codes)
+        ("bg", "3B"), #background color represented in 3 unsigned bytes (rgb color codes)
+    ]
+)
+
+tile_dt = np.dtype(
+    [
+        ("walkable", np.bool), #true if can walk over tile
+        ("transparent", np.bool), #true if this tile doesn't block FOV
+        ("dark", graphic_dt), #Graphics for when tile is not in FOV
+    ]
+)
+
+
+def new_tile(
+    *, #enforces use of keywords, so that parameter order doesn't matter
+    walkable: int,
+    transparent: int,
+    dark: Tuple[int, Tuple[int,int,int], Tuple[int,int,int]],
+) -> np.ndarray:
+    return np.array((walkable, transparent, dark), dtype=tile_dt)
+
+floor = new_tile(
+    walkable=True, transparent=True, dark=(ord("#"), (255,255,255), (50,50,150)),
+)
+
+wall = new_tile(
+    walkable=False, transparent=False, dark=(ord(" "), (255,255,255), (0,0,100)),
+)
