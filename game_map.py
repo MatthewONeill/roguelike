a0b0a3b77e56132cd6a4a503a4631291eea2aa69
Matthew <matthewjo30@gmail.com>
2020-10-10 23:27:53 -0400

    game map

--- game_map.py
+++ game_map.py
@@ -1,16 +1,29 @@
+from __future__ import annotations
+from typing import Iterable, Optional, TYPE_CHECKING
+
 import numpy as np
 from tcod.console import Console
 
 import tile_types
 
+if TYPE_CHECKING:
+    from entity import Entity
+
 class GameMap:
-    def __init__(self, width: int, height: int):
+    def __init__(self, width: int, height: int, entities: Iterable[Entity] = ()):
         self.width, self.height = width, height
+        self.entities = set(entities)
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F") #creates a 2D array filled with tile_types.wall
 
         self.visible = np.full((width, height), fill_value=False, order="F")
         self.explored = np.full((width, height), fill_value=False, order="F")
 
+    def get_blocking_entity_at_location(self, location_x: int, location_y: int) -> Optional[Entity]:
+        for entity in self.entities:
+            if entity.blocks_movement and entity.x == location_x and entity.y == location_y:
+                return entity
+
+        return None
 
     #Return true if x and y are in the bounds of the map
     def in_bounds(self, x: int, y: int) -> bool: 
@@ -22,3 +35,8 @@ class GameMap:
             choicelist=[self.tiles["light"], self.tiles["dark"]],
             default=tile_types.SHROUD
         )
+
+        for entity in self.entities:
+            # Only print entities that are in the FOV
+            if self.visible[entity.x, entity.y]:
+                console.print(x=entity.x, y=entity.y, string=entity.char, fg=entity.color)
